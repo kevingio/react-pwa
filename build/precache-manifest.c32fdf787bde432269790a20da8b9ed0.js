7eb73016c20e76dd38f862b4a77874e0690ac918
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2019-05-27 23:39:16 +0700

    update gitignore production config

---
+++ build/precache-manifest.c32fdf787bde432269790a20da8b9ed0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74df091131596210d744aa97910b8202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2c7973a2992f1875691",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d85a76a935624fc49bb6",
+    "url": "/static/js/2.be927833.chunk.js"
+  },
+  {
+    "revision": "b2c7973a2992f1875691",
+    "url": "/static/js/main.6e5ea282.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
